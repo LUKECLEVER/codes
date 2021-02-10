283e5030f894f413a1cab79a2d456a0653197af7
陆尧瓜瓜 <60529895+LUKECLEVER@users.noreply.github.com>
2021-02-10 09:07:03 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.write("<s"+"cript type='text/javascript' src='/js/test.js?"+Math.random()+"'></scr"+"ipt>"); 
+document.write("<s"+"cript type='text/javascript' src='https://codes.luyaoguagua.top/js/main.js?"+Math.random()+"'></scr"+"ipt>"); 
 /*
 	瓜瓜导航页 by 瓜瓜
 	luyaoguagua.top | admin@luyaoguagua.top
