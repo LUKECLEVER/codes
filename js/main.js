d457a5346ca619ffe99a6833a6cc12609cce9994
陆尧瓜瓜 <60529895+LUKECLEVER@users.noreply.github.com>
2021-02-10 09:15:48 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@
 			$window.on('load', function() {
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');//is-loading
-				}, 1000);//100
+				}, 1);//100
 			});
 
 		// Fix: Placeholder polyfill.
